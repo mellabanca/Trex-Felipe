945a8be2b6cabd77b6247238f695ad1f40039499
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-02-02 11:35:13 -0300

    02/02/2022

--- sketch.js
+++ sketch.js
@@ -1,31 +1,31 @@
 var player, playerAnim;
 var borda;
-
+var chao, chaoSpr;
 function preload(){
-  playerAnim = loadAnimation("./Imagens/trex1.png","./Imagens/trex3.png", "./Imagens/trex4.png");
-
+playerAnim = loadAnimation("./Imagens/trex1.png","./Imagens/trex3.png", "./Imagens/trex4.png");
+chaoSpr = loadImage("./Imagens/ground2.png");
 }
-
 function setup(){
-  createCanvas(600,200)
-  player = createSprite(50, 160, 20, 50);
-  player.addAnimation("correndo",playerAnim);
-  player.scale = 0.5;
-  borda = createEdgeSprites();
+createCanvas(600,200)
+player = createSprite(50, 160, 20, 50);
+player.addAnimation("correndo",playerAnim);
+player.scale = 0.5;
+borda = createEdgeSprites();
+chao = createSprite(200,180,400,20);
+chao.addImage("chao",chaoSpr);
+chao.x = chao.width/2;
 }
-
 function draw(){
-  background("white");
-
-  if(keyDown("space")){
-    player.velocityY = -10;
-  }
-
-
-  player.velocityY += 1;
-
-  player.collide(borda[3]);
-  
-  drawSprites();
+background("white");
+chao.velocityX = -2;
+if(chao.x < 0){
+chao.x = chao.width/2;
+}
+if(keyDown("space")){
+player.velocityY = -10;
+}
+player.velocityY += 1;
+player.collide(chao);
+drawSprites();
 }
 
