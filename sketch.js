2f24dd941c5c8ccf84fa1f89f23bb9ce6d57d596
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-03-02 14:20:59 -0300

    02/03/2022

--- sketch.js
+++ sketch.js
@@ -3,11 +3,24 @@ var borda;
 var chao, chaoSpr;
 var chaoInv;
 var nuvem, nuvemSpr;
+var obs1, obs2, obs3, obs4, obs5, obs6;
+var pontuacao;
+var grupoNuvens;
+var grupoObstaculos;
+var JOGANDO = 1;
+var ACABOU = 0;
+var estado = JOGANDO;
 
 function preload(){
     playerAnim = loadAnimation("./Imagens/trex1.png","./Imagens/trex3.png", "./Imagens/trex4.png");
     chaoSpr = loadImage("./Imagens/ground2.png");
     nuvemSpr = loadImage("./Imagens/cloud.png");
+    obs1 = loadImage("./Imagens/obstacle1.png");
+    obs2 = loadImage("./Imagens/obstacle2.png");
+    obs3 = loadImage("./Imagens/obstacle3.png");
+    obs4 = loadImage("./Imagens/obstacle4.png");
+    obs5 = loadImage("./Imagens/obstacle5.png");
+    obs6 = loadImage("./Imagens/obstacle6.png");
 }
 
 function setup(){
@@ -22,6 +35,9 @@ function setup(){
     chao.x = chao.width/2;
     chaoInv = createSprite(200,190,400,10);
     chaoInv.visible = false;
+    pontuacao = 0;
+    grupoNuvens =  new Group();
+    grupoObstaculos = new Group();
     //var numero = Math.round(random(1,100));
     //console.log(numero);
 }
@@ -30,7 +46,14 @@ function draw(){
     background("white");
     //console.log(frameCount);
     //console.log(player.y);
-    chao.velocityX = -2;
+
+    if(estado === JOGANDO){
+        chao.velocityX = -2;
+    } else if (estado === ACABOU){
+        chao.velocityX = 0;
+    }
+
+    
     if(chao.x < 0){
         chao.x = chao.width/2;
     }
@@ -40,7 +63,10 @@ function draw(){
     player.velocityY += 1;
     player.collide(chaoInv);
     nuvens();
-    drawSprites();
+    obstaculos();
+    drawSprites()
+    text(pontuacao,500,50);
+    pontuacao += Math.round(frameCount/60);
 }
 
 function nuvens(){
@@ -51,6 +77,37 @@ function nuvens(){
         nuvem.y = Math.round(random(1,150));
         nuvem.lifetime = 250;
         nuvem.depth = player.depth;
-        player.depth += 1;    
+        player.depth += 1;   
+        grupoNuvens.add(nuvem); 
+    }
+}
+
+
+function obstaculos(){
+    if(frameCount % 60 === 0){
+        var obs = createSprite(600,165,10,40);
+        obs.velocityX = -6;
+        var numeroAlt = Math.round(random(1,6));
+        switch(numeroAlt){
+            case 1: obs.addImage(obs1);
+            break;
+            case 2: obs.addImage(obs2);
+            break;
+            case 3: obs.addImage(obs3);
+            break;
+            case 4: obs.addImage(obs4);
+            break;
+            case 5: obs.addImage(obs5);
+            break;
+            case 6: obs.addImage(obs6);
+            break;
+            default:
+            break;
+        }
+
+        obs.scale = 0.5;
+        obs.lifetime = 250;
+
+        grupoObstaculos.add(obs);
     }
 }
